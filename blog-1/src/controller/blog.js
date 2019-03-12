2798b63794f536737b0473b33eab27c30baacd7b
GarenWang <2270099149@qq.com>
2019-03-13 07:54:28 +0800

    开发blog/detail接口

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -18,6 +18,18 @@ const getList = (author, keyword) => {
     ]
 }
 
+const getDetail = (id) => {
+    // 准备返回的假数据
+    return  {
+        id: 1,
+        title: '标题A',
+        content: "内容A",
+        createTime: new Date().getTime(),
+        author: '张三'
+    }
+}
+
 module.exports = {
     getList,
+    getDetail,
 }
