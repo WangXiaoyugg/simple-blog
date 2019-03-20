375d947ea3fe6c7a6f61ff0f4241261dc1ecbefb
GarenWang <2270099149@qq.com>
2019-03-20 22:25:03 +0800

    博客的接口增删改查

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -9,14 +9,22 @@ const {
     deleteBlog,
 } = require("../controller/blog")
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
 /* GET home page. */
 router.get('/list', function(req, res, next) {
-    const author = req.query.author || ''
-    const keyword = req.query.keyword || ''
+    let author = req.query.author || ''
+    let keyword = req.query.keyword || ''
     
     // 如果是管理员
     if(req.query.isadmin) {
+
+        // 不是管理员不让查看
+        console.log("req.session.username", req.session.username)
+        if(req.session.username == null) {
+            res.json(new ErrorModel('未登录'))
+            return;
+        }
 
         // 强制查询自己的博客
         author = req.session.username;
@@ -36,5 +44,38 @@ router.get('/detail', function(req, res, next) {
         res.json(new SuccessModel(detailData));
     })
 });
+
+router.post("/new", loginCheck,function(req,res, next) {
+    req.body.author = req.session.username;
+    let result = newBlog(req.body);
+    return result.then((blogId) => {
+        res.json(new SuccessModel(blogId));
+    })
+})
+
+router.post("/update", loginCheck,function(req,res, next) {
+    const result = updateBlog(req.query.id, req.body)
+    return result.then((flag) => {
+        if(flag) {
+            res.json(new SuccessModel());
+            return ;
+        }
+        res.json(new ErrorModel("更新博客失败"));
+    })
+})
+
+router.post("/del", loginCheck, function(req,res, next) {
+    let author = req.session.username;
+    const result = deleteBlog(req.query.id, author);
+    return result.then(flag => {
+        if(flag) {
+             res.json(new SuccessModel())
+        } else {
+            res.json(new ErrorModel("删除博客失败"))
+        }      
+    })
+})
+
+
 
 module.exports = router;
